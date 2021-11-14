9ba794c835ffb08a7407516e86ef26a2e84d9af0
WEIWENLAN <weiwenlan1999@gmail.com>
2021-11-14 14:49:46 -0500

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Wenlan Wei',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Wenlan Wei is aim to build models and services with AI.',
+    siteUrl: 'https://wenlan.me/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'WENLANWEI',
+        short_name: 'WENLANWEI',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
