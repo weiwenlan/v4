a472e0282da5e15b3c64ab96b473077a79f5fd02
WEIWENLAN <weiwenlan1999@gmail.com>
2021-11-15 12:37:03 -0500

    1

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
-const IconLoader = () => (
+const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <g id="B" transform="translate(11.000000, 5.000000)">
+      <g id="L" transform="translate(28.000000, 30.000000)">
         <path
           d="M33.3125 26.0312L39.8125 0.1875H45.5938L35.75 34H31.0625L22.8438 8.375L14.8438 34H10.1562L0.34375 0.1875H6.09375L12.75 25.5L20.625 0.1875H25.2812L33.3125 26.0312Z"
-          fill="#64FFDA"></path>
+          fill="#64FFDA"
+        />
       </g>
+
       <path
-        stroke="currentColor"
+        stroke="#64FFDA"
         strokeWidth="5"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
+                L 11, 27
+                L 11, 72
+                L 50, 95
+                L 89, 73
+                L 89, 28 z"
       />
     </g>
   </svg>
 );
 
-export default IconLoader;
+export default IconLogo;
